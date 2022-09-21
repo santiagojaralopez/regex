8838182026c3199a736e1abfacbc53380a09562c
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2022-09-20 19:13:01 -0500

    Finished Python example

--- python/main.py
+++ python/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import re
 
 #1872-11-30,Scotland,England,0,0,Friendly,Glasgow,Scotland,FALSE
@@ -10,8 +9,11 @@ f = open('./dataRegex.csv', 'r')
 for line in f:
   res = re.match(pattern, line)
   if res:
+    #Adding the goals of each team
     total = int(res.group(4)) + int(res.group(5))
     if total > 10:
+      #In case there are more than 10 goals, prints something like this:
+      #1882 | Northern Ireland: 0 - England: 13
       print(f'{res.group(1)} | {res.group(2)}: {res.group(4)} - {res.group(3)}: {res.group(5)}')
 
 f.close()
